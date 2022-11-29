9b00b98db899c82802a141068d60ccb5587996a3
TijsM <tijs.martens.tijs@gmail.com>
2022-11-29 13:51:43 +0100

    feat: remove Constants.installationId

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ export class ExpoMixpanelAnalytics {
 
     this.token = token;
     this.userId = null;
-    this.clientId = Constants.deviceId || Constants.installationId;
+    this.clientId = Constants.deviceId
     this.constants = {
       app_build_number: Constants.manifest?.revisionId,
       app_id: Constants.manifest?.slug,
